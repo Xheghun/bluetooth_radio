0fe30e6b48cb588aa3d71fd3726bc7aa577f7b6b
Xheghun <ysenruf@gmail.com>
2021-04-02 04:48:22 +0400

    few tweaks

--- lib/ui/flutter_blue.dart
+++ lib/ui/flutter_blue.dart
@@ -1,4 +1,4 @@
-import 'package:bluetooth_scanner/ui/res/color.dart';
+import 'package:bluetooth_scanner/res/color.dart';
 import 'package:bluetooth_scanner/ui/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
